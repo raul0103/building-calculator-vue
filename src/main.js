4c15ce3618b2beab1595d27631f0c79269b30b38
raul0103 <rshakurov95@mail.ru>
2023-06-28 03:42:11 +0600

    commit

--- src/main.js
+++ src/main.js
@@ -12,9 +12,6 @@ if (module.hot) {
 if (process.env.NODE_ENV === "development") {
   globalThis.__VUE_OPTIONS_API__ = true;
   globalThis.__VUE_PROD_DEVTOOLS__ = true;
-} else {
-  globalThis.__VUE_OPTIONS_API__ = false;
-  globalThis.__VUE_PROD_DEVTOOLS__ = false;
 }
 
 const app = createApp(App);
